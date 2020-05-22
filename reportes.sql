626aacb0ce41ad8375718ec7c6acf2a5e28631c5
Javier Eliud <javiereliudliz@gmail.com>
2020-05-22 12:51:52 -0700

    Muchas gracias Emiliano :D

---
+++ reportes.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-05-2020 a las 21:36:13
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reportes`
+--
+
+CREATE TABLE `reportes` (
+  `id` int(11) NOT NULL,
+  `numero_paciente` varchar(20) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellidos` varchar(255) DEFAULT NULL,
+  `nacimiento` date DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` varchar(20) DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL,
+  `presion` decimal(10,2) DEFAULT NULL,
+  `pulso` decimal(10,2) DEFAULT NULL,
+  `temperatura` decimal(10,2) DEFAULT NULL,
+  `observaciones` varchar(512) NOT NULL,
+  `cantidad` varchar(255) DEFAULT NULL,
+  `rodilla` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `reportes`
+--
+
+INSERT INTO `reportes` (`id`, `numero_paciente`, `nombre`, `apellidos`, `nacimiento`, `fecha`, `hora`, `estado`, `presion`, `pulso`, `temperatura`, `observaciones`, `cantidad`, `rodilla`, `created_at`, `updated_at`) VALUES
+(2, '12', 'Tarantula', 'Eliud', '1998-06-06', '2020-05-22', '10:00 am', 'vivo', '12.00', '12.00', '40.00', 'caca', '50 ml', 'Derecha', '2020-05-23 02:24:09', '2020-05-23 02:24:09'),
+(3, '13', 'Javier Eliud', 'Eliud', '1375-06-02', '2020-05-22', '11:00 am', 'vivo', '12.00', '12.00', '40.00', 'XD', '60 ml', 'Derecha', '2020-05-23 02:33:54', '2020-05-23 02:33:54');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `reportes`
+--
+ALTER TABLE `reportes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `reportes`
+--
+ALTER TABLE `reportes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
